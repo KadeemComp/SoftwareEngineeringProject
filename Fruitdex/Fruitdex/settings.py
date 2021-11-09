125a6dd56722fc093ca3b2294bd582d3f00be58b
Kadeem Compton <52226460+KadeemComp@users.noreply.github.com>
2021-11-09 16:25:08 -0400

    The basic environment is setup
    This Djando project has 2 apps currently.
    the first is the home app, which will have landing and browse catalog pages.
    the user app is where we will manage the user page and user database.
    
    To run the file:
    1. CD into the fruitdex folder or just open the folder in VS code by right clicking.
    2. in your terminal type the command "python manage.py runserver"
    3. to view the site.  enter the development IP address /Home to view the home page and /users to view the user page

--- Fruitdex/Fruitdex/settings.py
+++ Fruitdex/Fruitdex/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e6m*=0-zek0!+qp5bj(fa4ggke5q7($s^o!!9bv3fnwi)0sig='
+SECRET_KEY = 'django-insecure-#m!^26h)+u#6hhq1^b0r2yvbenb9wi)2j%kj5zcj8rt$g=!duf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Home',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
