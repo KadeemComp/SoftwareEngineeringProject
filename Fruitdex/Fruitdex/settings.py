b8646a110c3346daf4198d8ad0a8d02f230e434e
Kadeem Compton <52226460+KadeemComp@users.noreply.github.com>
2021-11-11 08:49:56 -0400

    Revert "Added environment variables"
    This reverts commit 796c66285ed2b65e966949fb18e53e891905be84.

--- Fruitdex/Fruitdex/settings.py
+++ Fruitdex/Fruitdex/settings.py
@@ -11,14 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
-import environ
 import django_heroku
 from pathlib import Path
-
-#Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,12 +122,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 
 
 # Extra places for collectstatic to find static files.
