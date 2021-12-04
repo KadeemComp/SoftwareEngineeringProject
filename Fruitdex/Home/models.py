c927a30f9473f2a1328bfb8d67d3059b384f96ee
Kadeem Compton <52226460+KadeemComp@users.noreply.github.com>
2021-12-03 23:32:34 -0400

    Minor changes made while debugging

--- Fruitdex/Home/models.py
+++ Fruitdex/Home/models.py
@@ -8,7 +8,7 @@ class Fruits(models.Model):
     fruit_name = models.CharField(max_length=100)
     #image = models.ImageField(default = 'default.jpg', upload_to='fruit_image')
     content = models.TextField()
-    date_posted = models.DateTimeField(default= timezone.now) #auto_now = True
+    date_posted = models.DateTimeField(default= timezone.now) 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
--- Fruitdex/Home/views.py
+++ Fruitdex/Home/views.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-#from .models import Fruits
 import pyrebase
-
+from .models import Fruits
 #dummy data of fruits
 
 fruits = [
