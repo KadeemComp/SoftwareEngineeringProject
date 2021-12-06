506ed1b7b92f751f515e930a999bfd0e81585cbe
DBcodezone <42975991+ESBcode@users.noreply.github.com>
2021-12-06 18:42:30 -0400

    Everything most go

--- Fruitdex/users/urls.py
+++
@@ -1,9 +0,0 @@
-from  django.urls import path
-from . import views
-
-
-# app_name = 'Users'
-urlpatterns = [
-    path("", views.user_profile,name= "profile"),
-    path("signup", views.signup_view, name ="signup")
-]
