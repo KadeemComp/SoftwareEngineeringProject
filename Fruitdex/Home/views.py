ab65c3dbeb1f799b63937b9c573049694094ea97
David <Bailey>
2021-12-06 17:34:15 -0400

    Update views.py

--- Fruitdex/Home/views.py
+++ Fruitdex/Home/views.py
@@ -10,24 +10,61 @@ from django.views.generic import (
     DeleteView
 )
 import pyrebase
+<<<<<<< Updated upstream
 from .models import Fruit
+=======
+import os
+from .models import Fruits
+#dummy data of fruits
+
+fruits = [
+    {
+       'name':'mango',
+       'scientific_name': 'something',
+       'content': ['Grenada - mango', 'Trinidad - mango'],
+       'author': 'jane doe',
+       'date_posted': 'December 2nd, 2018'
+    },
+    {
+       'name':'apple',
+       'scientific_name': 'somethingElse',
+       'content': ['Grenada - apple', 'Trinidad - apple'],
+       'author': 'john Doe',
+       'date_posted': 'December 21st, 2018'
+    },
+    {
+       'name':'apple',
+       'scientific_name': 'somethingElse',
+       'content': ['Grenada - apple1', 'Trinidad - apple1'],
+       'author': 'john1 Doe1',
+       'date_posted': 'December 31st, 2018'
+    },
+     {
+       'name':'apple',
+       'scientific_name': 'somethingElse',
+       'content': ['Grenada - apple3', 'Trinidad - apple4'],
+       'author': 'John Bob Doe',
+       'date_posted': 'December 5th, 2018'
+    }
+    ]
+>>>>>>> Stashed changes
 
 
 config = {
-    'apiKey': "AIzaSyBzdoeJ9nOpaCctqAvDuO5u5FfnruaeHZ0",
+    'apiKey': str(os.getenv('FIREBASE_API_KEY')), # stored in environment variables for security reasons
     'authDomain': "fruitdex-imgdb.firebaseapp.com",
     'databaseURL': "https://fruitdex-imgdb-default-rtdb.firebaseio.com",
     'projectId': "fruitdex-imgdb",
     'storageBucket': "fruitdex-imgdb.appspot.com",
-    'messagingSenderId': "174012390027",
-    'appId': "1:174012390027:web:21fd5be54bf72a55b7928e",
-    'measurementId': "G-CLPNV414GD"
 }
 firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
-storage = firebase.storage()
 
-# Create your views here.
+db = firebase.database()
+
+
+db.child("fruit").push(fruits) # pushing the dummie data to fire
+
+
 
 #here we have a response to a request which renders an HTML page
 def index(request):
@@ -38,8 +75,9 @@ def logo(request):
 
 
 @login_required
-def addfruit(request):
+def addfruit(request):  
     return render(request,"Home/addfruit.html")
+        
 
 
 def browse(request):
