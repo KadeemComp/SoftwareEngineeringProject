2c925824e8ff19713e248e8a7f9cb484bf6e72b3
Kadeem Compton <52226460+KadeemComp@users.noreply.github.com>
2021-12-11 22:21:42 -0400

    fresh start on the DB

--- Fruitdex/Home/models.py
+++ Fruitdex/Home/models.py
@@ -18,7 +18,7 @@ class Country(models.Model):
 class Fruit(models.Model):
     fruit_name = models.CharField(max_length=100)
     image = models.ImageField(default = 'default.jpg', upload_to='fruit_image')
-    country = models.ForeignKey(Country, null= True, on_delete=models.CASCADE)
+    country = models.ManyToManyField(Country)
     content = models.TextField()
     date_posted = models.DateTimeField(default= timezone.now) 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
